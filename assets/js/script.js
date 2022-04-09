8bab6b0164a57147194860af0dd5195e393d1527
Azurene <98633770+Azurene@users.noreply.github.com>
2022-04-09 16:55:25 -0700

    added styling to schedule blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,22 @@
 var currentDayEl = $("#currentDay");
 var date = Date()
+var timeblockEl = $("#timeblocks");
+
+// create time blocks for standard business hours
+// create color-coded time blocks for past-present-future
+// form handling for each time block
+// functions to save items into localStorage
 
 var displayDate = function() {
     currentDayEl.text(date);
-}
+};
 
+var scheduleForm = function() {
+
+    // create elements for time
+    // elements for time styling
+    // go through function that creates one timeblock for each hour of business
+};
 
 displayDate();
+scheduleForm();
